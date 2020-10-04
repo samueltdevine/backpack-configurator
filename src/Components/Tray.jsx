9651ec9b35d2a6e155afb5a6e5c46b1b8315a20c
Sam DeVine <sdevine.1@go.ccad.edu>
2020-10-04 15:30:43 -0400

    redux start

--- src/Components/Tray.jsx
+++ src/Components/Tray.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import {Backpacks, SleepingBags} from '../Items'
+import React from "react";
+import { Backpacks, SleepingBags } from "../Items";
 
-const Tray = () =>{
-
-
-
-    return <div className="trayContainer">
-        {SleepingBags.map((item)=>{
-           return <div className="trayItem">
-                <h5>{item.name}</h5>
-                <div className="trayImage"  style={{backgroundImage: `url(${item.image})`}} />
-            </div>
-        })}
+const Tray = () => {
+  return (
+    <div className="trayContainer">
+      {SleepingBags.map((item) => {
+        return (
+          <div className="trayItem">
+            <h5>{item.name}</h5>
+            <div
+              className="trayImage"
+              style={{ backgroundImage: `url(${item.image})` }}
+            />
+          </div>
+        );
+      })}
     </div>
-}
+  );
+};
 
-export default Tray
+export default Tray;
